11632d8319444dd998f8fb76b8b6b11deaadd711
Kriegslustig <github@ls7.ch>
2015-06-06 21:05:40 +0200

    Add a progress bar

--- static/js/upload.js
+++ static/js/upload.js
@@ -26,10 +26,17 @@ sendBack = (function () {
   return function (data) {
     var upload = new XMLHttpRequest()
     upload.open('POST', reciever, true)
-    upload.onload = renderDone.bind(null, upload)
+    upload.addEventListener('load', renderDone.bind(null, upload))
+    upload.addEventListener('progress', updateProgress.bind(null, upload))
     upload.send(data)
   }
 })()
+
+function updateProgress (xhrReq, evt) {
+  var progressBar = document.getElementsByClassName('progressBar')[0]
+  if(xhrReq.status && xhrReq.status !== 200) progressBar.style.backgroundColor = 'hsl(1, 60%, 80%)'
+  progressBar.style.height = ((evt.loaded / evt.total) * 100) + 'vh'
+}
 
 function renderDone (xhrReq, event) {
   document.getElementsByClassName('messages')[0].innerHTML = xhrReq.response
