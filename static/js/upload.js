f04493b5a23586570e5edbeb0135b8dc2be2e306
Kriegslustig <github@ls7.ch>
2015-06-06 18:20:07 +0200

    Implement a basic working version

---
+++ static/js/upload.js
@@ -0,0 +1,37 @@
+
+window.addEventListener('load', function () {
+  listenForFileUploads(document.getElementsByClassName('fileUploadForm')[0])
+})
+
+function listenForFileUploads (form) {
+  form.onsubmit = function (event) {
+    event.preventDefault()
+    uploadFileFromForm(form)
+    return false
+  }
+}
+
+function uploadFileFromForm (form) {
+  return uploadFile(form.getElementsByClassName('fileUpload')[0].files[0])
+}
+
+function uploadFile (file) {
+  var formData = new FormData()
+  formData.append('file', file, file.name)
+  sendBack(formData)
+}
+
+sendBack = (function () {
+  var reciever = 'upload'
+  return function (data) {
+    var upload = new XMLHttpRequest()
+    upload.open('POST', reciever, true)
+    upload.onload = renderDone.bind(null, upload)
+    upload.send(data)
+  }
+})()
+
+function renderDone (xhrReq, event) {
+  document.getElementsByClassName('messages')[0].innerHTML = xhrReq.response
+
+}
